4888d20b5154d14d7e0b2bf69093677660757aec
Pullchek <78024838+Pullchek@users.noreply.github.com>
2024-06-07 09:46:23 +0300

    App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const App = () => {
   const [backgroundColor, setBackgroundColor] = useState("rgb(25, 12, 118)");
-  {/* useState, React Hooks API'sinin bir parçasıdır ve React bileşenlerinde yerel durum (state) tutmak için kullanılır. */}
+  
 
   const onPressButton = () => {
     const randomColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
@@ -16,14 +16,10 @@ const App = () => {
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
-          {/* Eğer bir <Button> bileşenine border eklemek istiyorsanız, bunu doğrudan <Button> bileşenine uygulayamazsınız 
-            çünkü <Button> bileşeni stil özelliklerini doğrudan desteklemez */}
-      <TouchableOpacity style={styles.button} onPress={onPressButton}>
-        {/* React Native'de kullanıcı etkileşimleri için kullanılan bir bileşendir. Dokunmatik olayları (touch events) algılayarak
-           kullanıcıların bir bileşene dokunma veya üzerine basma gibi etkileşimlerini yakalar ve buna yanıt verir. */}
-        <Text style={styles.buttonText}>Click & Change Color</Text>
-      </TouchableOpacity>
-    </View>
+ <TouchableOpacity style={styles.button} onPress={onPressButton}>
+  <Text style={styles.buttonText}>Click Me Bro</Text>
+ </TouchableOpacity>
+  </View>
   );
 };
 
@@ -35,13 +31,13 @@ const styles = StyleSheet.create({
   },
   button: {
     borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 5,
-    padding: 10,
+    borderColor: "yellow",
+    borderRadius: 7,
+    padding: 12,
     backgroundColor: "white",
   },
   buttonText: {
-    fontSize: 16,
+    fontSize: 20,
     textAlign: "center",
   },
 });
